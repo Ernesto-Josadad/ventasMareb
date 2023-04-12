5fc2ecbd7a69b6be0cfbbcb7d9d9ccf56c38178c
Josadad <ernestcribanocanto@gmail.com>
2023-04-11 19:13:26 -0600

    git commit -m "Añadimos un conjunto de div"

--- resources/views/ventas.blade.php
+++ resources/views/ventas.blade.php
@@ -2,11 +2,23 @@
 @section('titulo','Interface de ventas')
 @section('contenido')
 
+<div id="apiVentas">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <input type="text" placeholder="Escriba el código del producto"
+                class="form-control">
+                <button class="btn-primary">Buscar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
 
 @push('scripts')
 <script type="text/javascript" src="js/vue-resource.js"></script>
-<script type="text/javascript" src="js/apis/apiMascota.js"></script>
+<script type="text/javascript" src="js/apis/apiVenta.js"></script>
 @endpush
 
 
